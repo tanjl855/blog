2efd894298d57768f9b296e7e66c8eb568806438
tanjl855 <tanjianliang855@163.com>
2023-10-24 01:04:48 +0800

    add .gitignore

--- cmd/main.go
+++ cmd/main.go
@@ -1,1 +1,22 @@
-package cmd
+package main
+
+import (
+	"github.com/gin-gonic/gin"
+	"net/http"
+)
+
+func main() {
+	r := gin.Default()
+	r.GET("/index", func(ctx *gin.Context) {
+		ctx.JSON(http.StatusOK, gin.H{
+			"code":    0,
+			"message": "Success",
+			"data": struct {
+				Data string `json:"data"`
+			}{
+				Data: "here is index msg",
+			},
+		})
+	})
+	r.Run()
+}
