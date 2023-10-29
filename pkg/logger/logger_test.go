b7ba7b2aba32ca28a9a60bb8c2dbe0e3e929a1ac
tanjl855 <tanjianliang855@163.com>
2023-10-30 01:26:19 +0800

    优化日志系统

---
+++ pkg/logger/logger_test.go
@@ -0,0 +1,23 @@
+package logger
+
+import (
+	"github.com/spf13/viper"
+	"os"
+	"testing"
+)
+
+func TestSetupLogger(t *testing.T) {
+	// windows test
+	viper.SetConfigFile("../.././configs/config.yaml")
+	err := viper.ReadInConfig()
+	if err != nil {
+		panic(err)
+	}
+	pwd, err := os.Getwd()
+	if err != nil {
+		Logger.Errorf("get pwd error: %v", err)
+		t.Error(err)
+	}
+	SetupLogger()
+	Logger.Infof("Here is test: %v", pwd)
+}
